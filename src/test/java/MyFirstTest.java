5d148761e038b92a415e01e353fa349bc02d4ea3
itsergpot <itsergpot@gmail.com>
2020-10-20 19:29:51 +0300

    improvements in tests

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,29 +1,39 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
+import static org.testng.Assert.assertEquals;
+
 public class MyFirstTest {
     private WebDriver driver;
+    private File file;
+    private static final String BUTTON_ID_1 = "one";
+    private static final String BUTTON_LABEL_1 = "One";
+    private static final String BUTTON_LABEL_2 = "Two";
 
     @BeforeTest
     public void setup(){
         driver = DriverFactory.getDriver(DriverType.CHROME);
+        file = new File("src/test/resources/example.html");
     }
 
     @Test
     public void firstTest(){
-        driver.get("C:\\Users\\itser\\IdeaProjects\\selenium-web-sample\\src\\main\\java\\example.html");
-        driver.findElement(By.id("one"));
+        driver.get(file.getAbsolutePath());
+
+        assertEquals(driver.findElements(By.id(BUTTON_ID_1)).size(), 1);
+        assertEquals(driver.findElement(By.id("one")).getAttribute("value"), BUTTON_LABEL_1);
     }
 
     @Test
     public void secondTest(){
-        driver.findElement(By.id("one")).click();
-        String textAfterClick = driver.findElement(By.id("one")).getAttribute("value");
-        Assert.assertEquals(textAfterClick, "Two");
+        driver.findElement(By.id(BUTTON_ID_1)).click();
+
+        assertEquals(driver.findElement(By.id(BUTTON_ID_1)).getAttribute("value"), BUTTON_LABEL_2);
     }
 
     @AfterTest
